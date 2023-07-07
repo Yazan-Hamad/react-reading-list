bafb62888f6edd807910e20a9d9a44581cecf8bd
Yazan Hamad <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-07 14:59:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
+function App() {
+  const [books, setBooks] = useState([]);
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books, { id: 123, title: title }
+    ];
+    setBooks(updatedBooks);
+  };
+
+  return <div>
+    {books.length}
+    <BookCreate onCreate={createBook} />
+  </div>;
+}
+
+export default App;
