87fdd2ba63afa64fdda519fbc15b7627b092abc8
Yazan-Hamad <yazanhamad999@gmail.com>
2023-07-08 18:29:40 +0300

    fetch books & edit book methods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () =>{
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  }
+
+  useEffect(()=>{
+    fetchBooks()},
+    []
+    );
+
   const createBook = async (title) => {
     const reponse = await axios.post('http://localhost:3001/books',{title});
     const updatedBooks = [
@@ -13,12 +24,13 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) =>{
+  const editBookById = async (id, newTitle) =>{
+    const reponse = await axios.put(`http://localhost:3001/books/${id}`,{title: newTitle});
     const updatedBooks = books.map((book)=>{
       if(book.id === id){
         return {
           ...books,
-          title: newTitle,
+          ...reponse.data,
         };
       };
       return book;
